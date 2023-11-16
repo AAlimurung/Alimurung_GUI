a0ac4cbb9ae1d1b562b4ca9c2bc6d7acc1ad67f5
LAB12_USER ON WS05 <L12Y16W05@citulab.net>
2023-11-16 17:07:57 +0800

    Commit All

--- src/FoodOrderSystem/FoodOrder.java
+++ src/FoodOrderSystem/FoodOrder.java
@@ -1,20 +1,57 @@
 package FoodOrderSystem;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class FoodOrder extends JFrame{
 
-    private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JCheckBox checkBox3;
-    private JCheckBox checkBox4;
-    private JCheckBox checkBox5;
-    private JCheckBox checkBox6;
+    private JCheckBox pizzaCheckBox;
+    private JCheckBox friesCheckBox;
+    private JCheckBox sundaeCheckBox;
+    private JCheckBox teaCheckBox;
+    private JCheckBox burgerCheckBox;
+    private JCheckBox softDrinksCheckBox;
+    private JPanel panel1;
+    private JRadioButton noneRadioButton;
+    private JRadioButton a5OffRadioButton;
+    private JRadioButton a10OffRadioButton;
+    private JRadioButton a15OffRadioButton;
+    private JButton orderButton;
 
     public FoodOrder(){
         FoodOrder am = this;
+        orderButton.addActionListener(e -> {
+            float sum = 0.0F;
+            float total;
+            if(pizzaCheckBox.isSelected()){
+                sum = sum + 100;
+            }
+            if(friesCheckBox.isSelected()){
+                sum = sum + 65;
+            }
+            if(burgerCheckBox.isSelected()){
+                sum = sum + 80;
+            }
+            if(softDrinksCheckBox.isSelected()){
+                sum = sum + 55;
+            }
+            if(teaCheckBox.isSelected()){
+                sum = sum + 50;
+            }
+            if(sundaeCheckBox.isSelected()){
+                sum = sum + 40;
+            }
 
+            if(a5OffRadioButton.isSelected()){
+                total = (float) (sum*0.05);
+                sum = sum - total;
+            } else if (a10OffRadioButton.isSelected()) {
+                total = (float) (sum*0.10);
+                sum = sum - total;
+            }else if(a15OffRadioButton.isSelected()){
+                total = (float) (sum*0.15);
+                sum = sum - total;
+            }
+            JOptionPane.showMessageDialog(am, "The total price is Php " + sum);
+        });
     }
 
     public static void main(String[] args) {
